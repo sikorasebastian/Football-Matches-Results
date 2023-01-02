4a2e50c73870e58977e5d6bca094a2f15fb06969
Vovul <vovul@tlen.pl>
2023-01-02 23:54:21 +0100

    Spring project initialized

---
+++ football/src/test/java/sport/springframework/football/FootballApplicationTests.java
@@ -0,0 +1,13 @@
+package sport.springframework.football;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
